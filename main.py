244743c37665595856592a9ff91e10a76cb07ac4
Alialloz <alialloz94450@gmail.com>
2024-08-30 18:08:53 -0400

    Implementation de la logique Back-end

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
+import os
 
-# Importing the required libraries for the application of PyQt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QComboBox, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QComboBox, QPushButton, QVBoxLayout, QWidget, QMessageBox
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("ExplorAI")
-        self.setGeometry(100, 100, 400, 200)
+        self.setGeometry(100, 100, 400, 300)  # Adjusted for potential error messages
 
         self.initUI()
 
@@ -25,7 +25,7 @@ class MainWindow(QMainWindow):
         layout.addWidget(labelFileName)
         layout.addWidget(self.lineEditFileName)
 
-        # Adding label and line edit for description
+        # Adding label and line edit for description (not used in search)
         labelDescription = QLabel("Description:", self)
         self.lineEditDescription = QLineEdit(self)
         self.lineEditDescription.setFixedHeight(50)  # Makes it a bit bigger
@@ -41,10 +41,26 @@ class MainWindow(QMainWindow):
 
         # Adding a push button to initiate file search
         self.pushButtonSearch = QPushButton("Cherche !", self)
+        self.pushButtonSearch.clicked.connect(self.searchFile)  # Connect button click to searchFile method
         layout.addWidget(self.pushButtonSearch)
 
         # Show the window
         self.show()
+
+    def searchFile(self):
+        # Get the file name and format from user input
+        fileName = self.lineEditFileName.text()
+        fileFormat = self.comboBoxFileFormat.currentText()
+
+        # Start from the root directory, adapt this to your requirement
+        rootDir = '/'
+        for root, dirs, files in os.walk(rootDir):
+            for file in files:
+                if file == fileName + fileFormat:
+                    QMessageBox.information(self, "File Found", f"File path: {os.path.join(root, file)}")
+                    return
+
+        QMessageBox.warning(self, "No Result", "File not found.")
 
 def main():
     app = QApplication(sys.argv)
